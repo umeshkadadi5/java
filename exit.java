0c1524047d3f437ba5732d7c83c469e6bad0ef15
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-18 22:01:24 +0530

    Method invocation commit

---
+++ exit.java
@@ -0,0 +1,12 @@
+class Humid{
+public static void main(String[] args){
+System.out.println("It's first");
+main();
+main();
+main();
+main();
+}
+public static void main(){
+System.out.println("it's second");
+}
+}
