a7da057b92aff6128e27cdffff6cebfb5df83c91
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-18 22:02:42 +0530

    Programs practised

---
+++ task1.java
@@ -0,0 +1,15 @@
+class Happy{
+public static void main(String[] args){
+System.out.println("It's Sunny");
+music();
+}
+public static void music()
+{
+System.out.println("Music brings peace of mind");
+game();
+}
+public static void drinking(){
+System.out.println("Drinking is passion");
+drinking();
+}
+}
---
+++ task2.java
@@ -0,0 +1,16 @@
+class Sleepy{
+public static void main(String[] args){
+System.out.println("It's Sunny");
+write();
+write();
+write();
+}
+public static void main(){
+System.out.println("done");
+}
+public static void write()
+{
+System.out.println("Music brings peace of mind");
+}
+
+}
