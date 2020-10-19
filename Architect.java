2505c72a66d5e974a1ec44b818f78b5374a0ba88
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-19 22:25:12 +0530

    Add files via upload

---
+++ Architect.java
@@ -0,0 +1,21 @@
+public class Architect{
+public static void main(String[] design)
+{
+byte fingers=5;
+short daysInAYear=365;
+int numberOfEnginersInIndia=4000000;
+long populationOfIndia=1350000000;
+float percentageOfStudent=85;
+double chargeOfElectron=1.60217662;
+char g=80;
+boolean earthIsFlat=false;
+System.out.println(fingers);
+System.out.println(daysInAYear);
+System.out.println(numberOfEnginersInIndia);
+System.out.println(populationOfIndia);
+System.out.println(percentageOfStudent);
+System.out.println(chargeOfElectron);
+System.out.println(g);
+System.out.println(earthIsFlat);
+}
+} 
