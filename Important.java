e664836e965ca4e99893476cbfb4c4139ce95e49
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-20 19:36:07 +0530

    task done

---
+++ Important.java
@@ -0,0 +1,12 @@
+public class Important{
+public static void main(String[] detail)
+{
+	final long adharNumber= 1234567890;
+	final long phoneNumber=1234567890;
+	final String mail="uk20.mr@gmail.com";
+
+System.out.println(adharNumber);
+System.out.println(phoneNumber);
+System.out.println(mail);
+}
+}
