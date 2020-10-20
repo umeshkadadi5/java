8d8976ba3c2456eb2ff1607714e61db74124b83e
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-20 19:33:18 +0530

    Practised thing

---
+++ Cloudy.java
@@ -0,0 +1,17 @@
+public class Cloudy{
+public static void main(String[] rain)
+{
+printMail();
+printProgrammingLanguage("Java"); printProgrammingLanguage("Python");
+printProgrammingLanguage("C"); printProgrammingLanguage("C++");
+printProgrammingLanguage("C#");
+}
+public static void printMail()
+{
+	System.out.println("uk20.mr@gmail.com");
+}
+public static void printProgrammingLanguage(String language)
+{
+	System.out.println("ProgrammingLanguage : "+ language);
+}
+}
