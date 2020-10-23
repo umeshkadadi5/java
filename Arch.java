7a597fe1f404841c3b4b59c029f0d64331900344
Umesh Kadadi <71939568+umeshkadadi5@users.noreply.github.com>
2020-10-23 21:49:39 +0530

    practise commit

---
+++ Arch.java
@@ -0,0 +1,12 @@
+public class Arch{
+public static void design(String[] employees)
+{
+int numberOfEmployees=employees.length;
+System.out.println("count of employees: " +numberOfEmployees);
+
+System.out.println("element at index 0: " + employees[0]);
+System.out.println("element at index 1: " + employees[1]);
+System.out.println("element at index 2: " +employees[2]);
+System.out.println("element at index 3: " +employees[3]);
+}
+}
---
+++ Company.java
@@ -0,0 +1,8 @@
+public class Company{
+public static void main(String[] office)
+{
+System.out.println("JVM invokes main method");
+Arch.design(office);
+System.out.println("exit from main");
+}
+}
